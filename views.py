d994df06d115e6e2a79c74177e3f39b7d072497d
navoday-91 <navoday.91@gmail.com>
2017-09-08 14:59:21 -0700

    PyCharm Test

--- views.py
+++ views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hi there! This is the test page for myapp")
+    return render(request, 'index.html')
