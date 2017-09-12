d580ae9a2702c0573a12dd9a8c85c72684a32ba8
navoday-91 <navoday.91@gmail.com>
2017-09-12 13:07:32 -0700

    PyCharm Test

--- urls.py
+++ urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
     url(r'^expression$', views.expression),
+    url(r'^expression/$', views.expression),
 	url(r'^details/(?P<todo_id>\w{0,50})/$', views.details)
 	]
