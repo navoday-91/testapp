1e55045d856afe86aac5be03b7f9a25a8a5be0a3
navoday-91 <navoday.91@gmail.com>
2017-09-12 13:01:42 -0700

    PyCharm Test

--- views.py
+++ views.py
@@ -9,6 +9,50 @@ def index(request):
     }
     return render(request, 'index.html', context )
 
+def compile_formula(formula, verbose=False):
+    """Compile formula into a function. Also return letters found, as a str,
+    in same order as parms of function. The first digit of a multi-digit
+    number can't be 0. So if YOU is a word in the formula, and the function
+    is called with Y eqal to 0, the function should return False."""
+
+    # modify the code in this function.
+
+    letters = ''.join(set(re.findall('[A-Z]', formula)))
+    firstletters = set(re.findall(r'\b([A-Z])[A-Z]', formula))
+    parms = ', '.join(letters)
+    tokens = map(compile_word, re.split('([A-Z]+)', formula))
+    body = ''.join(tokens)
+    if firstletters:
+        tests = ' and '.join(L + '!=0' for L in firstletters)
+        body = '%s and %s' % (tests, body)
+    f = 'lambda %s: %s' % (parms, body)
+    if verbose: print(f)
+    return eval(f), letters
+
+
+def compile_word(word):
+    """Compile a word of uppercase letters as numeric digits.
+    E.g., compile_word('YOU') => '(1*U+10*O+100*Y)'
+    Non-uppercase words uncahanged: compile_word('+') => '+'"""
+    if word.isupper():
+        terms = [('%s*%s' % (10 ** i, d))
+                 for (i, d) in enumerate(word[::-1])]
+        return '(' + '+'.join(terms) + ')'
+    else:
+        return word
+
+
+def faster_solve(formula):
+    f, letters = compile_formula(formula)
+    for digits in itertools.permutations((1, 2, 3, 4, 5, 6, 7, 8, 9, 0), len(letters)):
+        try:
+            if f(*digits) is True:
+                table = str.maketrans(letters, ''.join(map(str, digits)))
+                return formula.translate(table)
+        except ArithmeticError:
+            pass
+
+
 def details(request, todo_id):
     todo = myapp.objects.get(id = todo_id)
     context = {
@@ -17,5 +61,11 @@ def details(request, todo_id):
     return render(request, 'details.html', context)
 
 def expression(request):
-    return render(request, 'expression.html')
+    if request.method == 'POST':
+        expressionstring = request.POST['expression']
+        solution = faster_solve(expressionstring)
+        context = {
+            'solution': solution
+        }
+    return render(request, 'expression.html', context)
 
